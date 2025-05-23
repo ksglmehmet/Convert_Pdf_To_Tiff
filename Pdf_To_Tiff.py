beda3057ea42a3c2d7c81404ca18346fa7037b5b
ksglmehmet <mak.kosoglu@gmail.com>
2025-05-23 17:26:09 +0300

    Add PDF to TIFF conversion script and update README and .gitignore
    - Implemented a script to convert PDF files to TIFF format.
    - Updated README.md with usage instructions and requirements.
    - Added necessary files to .gitignore.

---
+++ Pdf_To_Tiff.py
@@ -0,0 +1,33 @@
+# Gerekli Kütüphaneleri kuralım.
+# pip install pdf2image, Image
+
+from pdf2image import convert_from_path
+from PIL import Image
+import paths as ph # Dizin bilgilerimi import ediyorum. 
+
+Image.MAX_IMAGE_PIXELS = None # Max pixels sınırı defaultta var. Eğer bu sınırı geçen pdf leriniz varsa, Tiff formatına dönüşemeyecek. Bu sebep ile bu sınırı kaldırıyoruz.
+
+# pdf_path değişkenine, pdflerinizin bulunduğu dizini verin.
+# output_path değişkenine, tiff formatında kaydedilecek dizini verin.
+
+# for döngüsündeki range değerlerini, pdf dosyalarınızın isimlendirilmesine göre ayarlayın. İsimlendirmeniz sayılarlaysa çok daha rahat olur.
+# Pdflerinizin bulunduğu dizinin öğe sayısınıda verebilirsiniz. Tamamen sizin pdflerinizin isimlendirilmesine bağlı.
+
+for i in range(20230000091494, 20230000098798):
+    pdf_path = f"{ph.pdf_dir}/{i + 1}.pdf"
+    output_path = f"{ph.output}/{i + 1}.tiff"
+    try:
+        images = convert_from_path(pdf_path, dpi=300, poppler_path="C:/poppler-24.08.0/Library/bin")
+        images[0].save(output_path, save_all=True, append_images=images[1:], compression="tiff_deflate")
+        print(f"{output_path} dosyası başarıyla oluşturuldu.")
+    except Exception as e:
+        print(f"{pdf_path} dosyası işlenemedi: {e}")
+        continue
+
+# Bu kod, belirtilen dizindeki PDF dosyalarını TIFF formatına dönüştürür.
+# Her PDF dosyası için, TIFF dosyası belirtilen dizine kaydedilir.
+# Kodun sonunda, her bir PDF dosyasının başarıyla dönüştürüldüğünü veya hata mesajını yazdırır.
+# Range aralığında belirtilen sayılar, PDF dosyalarının isimlendirilmesine göre ayarlanmalıdır.
+# Range aralığında olmayan belgelerim olduğu için, hata verecek pdf isimlerinde, try-except bloğu ile hata alındığında devam edilmesini sağlıyorum.
+# Not: Kodun çalışabilmesi için pdf2image ve Pillow kütüphanelerinin kurulu olması gerekmektedir.
+# Not: Kodun çalışabilmesi için poppler kütüphanesinin bilgisayarınızda kurulu olması gerekmektedir.
--- README.md
+++ README.md
@@ -8,3 +8,9 @@ PDF veya PDF'lerinizi Tiff dokümanına çevirmek için 20 satırlık kod işini
 indirmek için : https://github.com/oschwartz10612/poppler-windows/releases/tag/v24.08.0-0
 
 Release 24.08.0-0 indirebilir, dışarıya aktarıp, C veya herhangi bir dizine yerleştirebilirsiniz.
+
+Bu kod, belirtilen dizindeki PDF dosyalarını TIFF formatına dönüştürür.
+Her PDF dosyası için, TIFF dosyası belirtilen dizine kaydedilir.
+Kodun sonunda, her bir PDF dosyasının başarıyla dönüştürüldüğünü veya hata mesajını yazdırır.
+Not: Kodun çalışabilmesi için pdf2image ve Pillow kütüphanelerinin kurulu olması gerekmektedir.
+ Not: Kodun çalışabilmesi için poppler kütüphanesinin bilgisayarınızda kurulu olması gerekmektedir.
